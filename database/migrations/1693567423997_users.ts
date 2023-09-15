0625685eb2597d34a6b66444941834dd3f46d4ea
LuisGustavo16 <guluisgustavo300@gmail.com>
2023-09-15 08:38:42 -0300

    chatao

--- database/migrations/1693567423997_users.ts
+++ database/migrations/1693567423997_users.ts
@@ -6,7 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable().unique()
+      table.string('name', 80).notNullable()
+      table.string('email', 255).unique().notNullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
