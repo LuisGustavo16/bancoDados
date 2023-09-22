589d97de41356cee2e9132447ef51301e38ec847
LuisGustavo16 <guluisgustavo300@gmail.com>
2023-09-22 08:31:10 -0300

    Quase lá

--- start/routes.ts
+++ start/routes.ts
@@ -3,3 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/lista', async () => {
   return { hello: 'world' }
 })
+
+Route.post("/register", "AuthController.register")
+Route.post("/login", "AuthController.login")
